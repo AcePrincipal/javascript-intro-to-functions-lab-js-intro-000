939961d615124b959c5a2836607d6bc875683e1b
Alexander Petrov <acepetrov96@gmail.com>
2020-09-04 23:30:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiGrandma(string){
-  if (string.toUpperCase === string){
-    return "YES INDEED!";
-  } else if (string.toLowerCase() === string){
+  if (string.toLowerCase() === string){
     return "I can't hear you!";
+  }
+  } else if (string.toUpperCase === string){
+  return "YES INDEED!";
   } else {
     return "I love you too.";
   }
